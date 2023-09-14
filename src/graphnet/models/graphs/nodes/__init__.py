dd48a95c3165a6c69cd6d0f0d7e3c8750bdc860c
amhermansen <mail@andreashermansen.dk>
2023-09-14 11:59:15 +0200

    Added MircoSummaryNodes

--- src/graphnet/models/graphs/nodes/__init__.py
+++ src/graphnet/models/graphs/nodes/__init__.py
@@ -7,3 +7,4 @@ and their features.
 
 from .nodes import NodeDefinition, NodesAsPulses
 from .cropped_nodes import PulsesCroppedRandomly, PulsesCroppedValue
+from .summary_nodes import MircoSummaryNodes
--- src/graphnet/models/graphs/nodes/summary_nodes.py
+++ src/graphnet/models/graphs/nodes/summary_nodes.py
@@ -23,6 +23,10 @@ class MircoSummaryNodes(NodeDefinition):
     _charge_index = 4
     _first_time_index = 100 / 3.0e4
     _second_time_index = 500 / 3.0e4
+    _charge_scale = 2
+    _charge_quantile_times = np.array([0.2, 0.5])
+
+    _generated_features = 9 + len(_charge_quantile_times)
 
     @save_model_config
     def __init__(
@@ -30,7 +34,6 @@ class MircoSummaryNodes(NodeDefinition):
         times: Optional[List[float]] = None,
         time_quantiles: Optional[List[float]] = None,
         dom_specifiers: Optional[List[int]] = None,
-        index_permutation: Optional[List[int]] = None,
         **kwargs: Any,
     ):
         """Create per DOM summary variables.
@@ -52,57 +55,62 @@ class MircoSummaryNodes(NodeDefinition):
         else:
             self.dom_specifiers = np.array(dom_specifiers)
 
-        if index_permutation is not None:
-            self.index_permutation = np.empty_like(index_permutation)
-            self.index_permutation[index_permutation] = np.arange(
-                len(index_permutation)
+    def _construct_nodes(self, x: torch.Tensor) -> Data:
+        dom_separated_pulsemap = self._split_pulsemap_to_doms(x)
+        summary_data = np.empty(
+            (len(dom_separated_pulsemap), self._generated_features)
+        )
+        for idx, (key, dom_time_series) in enumerate(
+            dom_separated_pulsemap.items()
+        ):
+            charge_cumulative = np.cumsum(
+                dom_time_series[:, self._charge_index]
             )
-        else:
-            self.index_permutation = index_permutation
-
-    def _construct_nodes(self, x: torch.Tensor) -> Any:
-        dom_separated_pulsemap = self._split_pulse_map_to_doms(x)
-        summary_data = []
-        for key, dom_time_series in dom_separated_pulsemap.items():
-            summary_data.append(
-                np.array(
-                    [
-                        dom_time_series[0, self._x_index],
-                        dom_time_series[0, self._y_index],
-                        dom_time_series[0, self._z_index],
-                        t_min := np.min(dom_time_series[:, self._time_index]),
-                        self.max(dom_time_series[:, self._time_index]),
-                        np.mean(dom_time_series[:, self._time_index]),
-                        np.std(dom_time_series[:, self._time_index], ddof=1),
-                        np.log10(
-                            1 + np.sum(dom_time_series[:, self._charge_index])
-                        ),
-                        np.log10(
-                            1
-                            + np.sum(
-                                dom_time_series[
-                                    dom_time_series[:, self._time_index]
-                                    < t_min + self._first_time_index,
-                                    self._charge_index,
-                                ]
-                            )
-                        ),
-                        np.log10(
-                            1
-                            + np.sum(
-                                dom_time_series[
-                                    dom_time_series[:, self._time_index]
-                                    < t_min + self._second_time_index,
-                                    self._charge_index,
-                                ]
-                            )
-                        ),
-                    ]
-                )
+            charge_deposit_indices = np.searchsorted(
+                charge_cumulative, self._charge_quantile_times
+            )
+            summary_data[idx, :] = np.array(
+                [
+                    dom_time_series[0, self._x_index],
+                    dom_time_series[0, self._y_index],
+                    dom_time_series[0, self._z_index],
+                    t_min := np.min(dom_time_series[:, self._time_index]),
+                    self.max(dom_time_series[:, self._time_index]),
+                    np.mean(dom_time_series[:, self._time_index]),
+                    np.std(dom_time_series[:, self._time_index], ddof=1),
+                    np.log10(
+                        1 + np.sum(dom_time_series[:, self._charge_index])
+                    )
+                    / self._charge_scale,  # Total charge
+                    np.log10(
+                        1
+                        + np.sum(
+                            dom_time_series[
+                                dom_time_series[:, self._time_index]
+                                < t_min + self._first_time_index,
+                                self._charge_index,
+                            ]
+                        )
+                    ),  # Charge in first window
+                    np.log10(
+                        1
+                        + np.sum(
+                            dom_time_series[
+                                dom_time_series[:, self._time_index]
+                                < t_min + self._second_time_index,
+                                self._charge_index,
+                            ]
+                        )
+                    ),  # Charge in second window
+                    *[
+                        dom_time_series[charge_index, self._charge_index]
+                        for charge_index in charge_deposit_indices
+                    ],
+                ]
             )
         pass
 
-    def _split_pulse_map_to_doms(self, x: torch.Tensor) -> Any:
+    def _split_pulsemap_to_doms(self, x: torch.Tensor) -> Any:
         x = x.numpy()
         if self.index_permutation is not None:
             x_sort = x[:, self.index_permutation]
