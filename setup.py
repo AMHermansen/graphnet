5dcf9f1daea14475b90ccd546d6b3039a1b08e95
Andreas Michael Hermansen <97125645+AMHermansen@users.noreply.github.com>
2024-04-11 13:40:23 +0200

    Add Python 3.12 to classifiers

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
     "Environment :: CPU",
     "Environment :: GPU",
     "License :: OSI Approved :: Apache Software License",
